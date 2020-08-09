8016099ac6bad40f97b8044885a216c45384dbc5
abeker <acabeker@gmail.com>
2020-08-09 11:54:53 +0200

    project created

---
+++ src/main/java/com/owasp/owasp/OwaspApplication.java
@@ -0,0 +1,13 @@
+package com.owasp.owasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwaspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwaspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owasp/owasp/OwaspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owasp.owasp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwaspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
